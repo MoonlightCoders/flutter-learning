f315497a403b1b1f2cadd0d35a3471e8f6224de7
Gowtham <54016506+gowtham-xseed@users.noreply.github.com>
2021-08-01 11:29:46 +0530

    State full widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,50 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
-          body: Text('gowtham'),
+          body: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Temp(),
+              ],
+            ),
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class Temp extends StatefulWidget {
+  const Temp({Key? key}) : super(key: key);
+
+  @override
+  _TempState createState() => _TempState();
+}
+
+class _TempState extends State<Temp> {
+  int value = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text(value.toString()),
+          TextButton(
+            onPressed: () {
+              setState(() {
+                value = value + 1;
+              });
+            },
+            child: Text('Click'),
+          )
+        ],
       ),
     );
   }
